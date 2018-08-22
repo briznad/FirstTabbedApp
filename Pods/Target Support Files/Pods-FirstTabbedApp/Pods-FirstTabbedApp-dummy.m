8ddb9bea71be276958d79337b8d02bbf9011b0a2
Brad Mallow <brad.mallow@gmail.com>
2018-08-22 16:43:23 -0700

    remove cocoa pods

--- Pods/Target Support Files/Pods-FirstTabbedApp/Pods-FirstTabbedApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FirstTabbedApp : NSObject
-@end
-@implementation PodsDummy_Pods_FirstTabbedApp
-@end
--- Pods/Target Support Files/Pods-FirstTabbedAppUITests/Pods-FirstTabbedAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FirstTabbedAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FirstTabbedAppUITests
-@end
